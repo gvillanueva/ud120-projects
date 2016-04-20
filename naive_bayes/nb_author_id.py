b12ddf18989c3d4cb8ba1e3ba95ffbfcf2f2f75b
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-04-20 07:07:30 -0700

    Complete code for Naive-Bayes mini project
    Creates a GaussianNB object and trains it with the given data. Prints time
    required to train and predict, and prints accuracy estimated using
    sklearn.metrics.accuracy_score().

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+# Create a Gaussian Naive Bayes classifier
+clf = GaussianNB()
+
+# Train the new classifier with our features/labels training data
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+# Use the classifier to predict the labels of the test data
+t0 = time()
+labels_test_pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0, 3), "s"
+
+# Import the accuracy score measurement method from metrics module
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, labels_test_pred)
 
 #########################################################
 
