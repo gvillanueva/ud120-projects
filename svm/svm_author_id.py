5b52387a102fb8d683a836358867a64f01dae033
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-04-20 09:39:56 -0700

    Complete tasks for second mini project using SVM
    Creates a Support Vector Machine and trains it on our sample data. Prints
    some information about the training/prediction time and outputs a few
    particular bits of data to satisfy questions from instructors.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,10 +20,29 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
+# Import support vector classifier
+from sklearn.svm import SVC
+
+# Create support vector classifier
+clf = SVC(kernel="rbf", C=10000.0)
+
+# Train our classifier and print how long it too to train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+# Make predictions using the classifier
+t0 = time()
+labels_pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0, 3), "s"
+print labels_pred[10]
+print labels_pred[26]
+print labels_pred[50]
+print sum(labels_pred)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, labels_pred)
 
 #########################################################
 
