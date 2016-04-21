b8f42376f1b4a521e74e1d48b718bea3c240c7ed
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-04-21 13:35:32 -0700

    Complete tasks for Decision Tree mini-project
    Creates a Decision Tree classifier and trains it with the provided training
    data. Uses provided testing data to create predictions, and outputs the
    calculated accuracy score.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -23,9 +23,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
+labels_pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, labels_pred)
+print acc
 #########################################################
 
 
