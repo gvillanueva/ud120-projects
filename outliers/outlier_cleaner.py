e4b5116fdc8344501a1783fff3e90679a2df830f
Giancarlo Villanueva <cvillanueva@graphicproducts.com>
2016-04-26 08:10:38 -0700

    Complete outliers mini-project using Enron data

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+def sortResidualError(entry):
+    return entry[0] - entry[2]
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -12,9 +14,17 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
-    ### your code goes here
-
     
-    return cleaned_data
+    ### your code goes here
+    # create new list of residual errors
+    errors = predictions - net_worths
+
+    # combine relevant values into a list of tuples
+    cleaned_data = zip(ages, net_worths, errors)
+    
+    # sort combined values by errors ascending
+    cleaned_data = sorted(cleaned_data, key=lambda x: x[2])
+    
+    # return slice with largest 10% residual error removed
+    return cleaned_data[:-int(len(cleaned_data) * 0.1)]
 
