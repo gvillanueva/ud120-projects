856690b4bdcbdf4cf137a9496276dc99b0ecc2c1
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2019-09-02 11:07:17 -0500

    Complete Evaluation Metrics mini-project
    Find tn, fp, fn, tp of the split training/testing data set and for the
    quiz lists.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,23 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.3, random_state = 42)
 
+clf = DecisionTreeClassifier()
+clf = clf.fit(train_features, train_labels)
+pred = clf.predict(test_features, test_labels)
+print len(test_features)
+print pred
+
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+print precision_score(pred, test_labels)
+print recall_score(pred, test_labels)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+tn, fp, fn, tp = confusion_matrix(true_labels, predictions).ravel()
